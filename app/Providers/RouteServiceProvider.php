a88a71d3ae6f2cbfad6b22bfc425273d8946e374
arafat-web <arafat.122260@gmail.com>
2024-02-11 18:59:08 +0600

    update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -36,8 +36,7 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
             
-            Route::middleware('web')
-                ->prefix('admin')
+            Route::prefix('admin')
                 ->group(base_path('routes/admin.php'));
         });
     }
